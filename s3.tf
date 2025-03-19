0a3e1bcc7202816b84f1e4ebab8043619eaae08d
msoumahoro <aisha.kenneh@gmail.com>
2025-03-18 20:48:53 -0500

    Removed ignored files from repo

--- s3.tf
+++
@@ -1,6 +0,0 @@
-# Reference an existing S3 bucket instead of creating a new one
-data "aws_s3_bucket" "existing_bucket" {
-  bucket = "terraform-homework-s3-bucket123" # Use the existing bucket name
-}
-
-
